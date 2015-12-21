56e2b669549c4b179c17d2ce94ac92e4fc54fd02
bluw <bluw.ced@gmail.com>
2015-12-21 10:47:24 +0100

    final version

--- WebApplication/src/java/converters/PriceConverter.java
+++ WebApplication/src/java/converters/PriceConverter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controllerManagedBeans;
+package converters;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
--- WebApplication/src/java/validators/PasswordValidator.java
+++ WebApplication/src/java/validators/PasswordValidator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controllerManagedBeans;
+package validators;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
